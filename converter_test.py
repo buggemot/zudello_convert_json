81963f1790a0b83b704f52db5c59b396ad0c1812
Alexander <buggemot@gmail.com>
2020-02-10 09:31:29 +0900

    Add split camel case function

--- converter_test.py
+++ converter_test.py
@@ -11,3 +11,18 @@ def test_add_extension():
     assert converter.add_extension(test_filename, '') == 'test'
     assert converter.add_extension(test_filename2, 'json') == 'test.json'
     assert converter.add_extension(test_filename3, 'txt') == 'test.txt'
+
+def test_split_camel_case():
+
+    test = "test"
+    expected_test = "test"
+
+    test2 = "teStstrInG"
+    expected_test2 = "te Ststr In G"
+
+    test3 = ""
+    expected_test3 = ""
+
+    assert converter.split_camel_case(test) == expected_test
+    assert converter.split_camel_case(test2) == expected_test2
+    assert converter.split_camel_case(test3) == expected_test3
--- release/converter.py
+++ release/converter.py
@@ -1,70 +1,78 @@
-import pdb
+"""
+Script to Convert JSON object to Zudello interface
+"""
+#import pdb
 import logging
-import utils
-import os
-import rules
 import re
+import os
+import utils
+import rules
 
-def convert_to_interface(json_content, logger):
-    out_interface = []
 
-    def iterate_group_node(node, group_node):
-        for key in node:
-            if isinstance(node[key], str):
-                single_node = {
-                    "name": key,
-                    "label": key,
-                    "type": get_type_of_parameter(key, node[key])
-                }
-                group_node['spec'].append(single_node)
-        return group_node
-
-    first_level_key = [k for k in json_content.keys()][0]
-    for node in json_content[first_level_key]:
-        for key in node:
-            if isinstance(node[key], dict):
-                group_node = {
-                    "name": key,
-                    "label": key,
+def generate_out_interface(json_content, node):
+    #pdb.set_trace()
+    for k, v in json_content.items():
+        if isinstance(v, dict):
+            if True:
+                dict_node = {
+                    "name": k,
+                    "label": k,
                     "type": "array",
                     "spec": []
                 }
-                out_node = iterate_group_node(node[key], group_node)
-                out_interface.append(out_node)
-            elif isinstance(node[key], list):
-                for item in node[key]:
-                    group_node = {
-                        "name": key,
-                        "label": key,
-                        "type": "collection",
+            generate_out_interface(v, dict_node['spec'])
+            node.append(dict_node)
+        elif isinstance(v, list):
+            for i in v:
+                if True:
+                    list_node = {
+                        "name": k,
+                        "label": k,
+                        "type": "collections",
                         "spec": []
                     }
-                    out_node = iterate_group_node(item, group_node)
-                    out_interface.append(out_node)
-            else:
-                single_node = {
-                    "name": key,
-                    "label": key,
-                    "type": get_type_of_parameter(key, node[key])
-                }
-                out_interface.append(single_node)
-        return out_interface
+                    generate_out_interface(i, list_node['spec'])
+                    node.append(list_node)
+        else:
+            single_node = {
+                "name": k,
+                "label": k,
+                "type": get_type_of_parameter(k, v)
+            }
+            node.append(single_node)
+    return node
 
 
 def get_type_of_parameter(key, value):
+    """
+    Return type of value using rules
+    """
     for rule in rules.rule_for_single_parameter:
         parameter, rule_for_key, rule_for_parameter = rule
         if rule_for_key:
             is_search_key = re.search(rule_for_key, key, re.IGNORECASE) or False
         if rule_for_parameter:
+            if isinstance(value, str) and len(value) == 0:
+                return "text"
+            if not (isinstance(value, str) or isinstance(value, bytes)):
+                value = str(value)
             is_match_parameter = re.match(rule_for_parameter, value, re.IGNORECASE) or False
-        if is_search_key or is_match_parameter:
-            return parameter
+            if is_search_key or is_match_parameter:
+                return parameter
     return "unknown"
 
 
-def main():
+def add_extension(filename, extension):
+    if filename.endswith(extension):
+        return filename
+    else:
+        return "{}.{}".format(filename, extension)
 
+
+def main():
+    """
+    main function
+    """
     in_dir = "in"
     utils.is_exists_dir(in_dir, True)
 
@@ -75,7 +83,7 @@ def main():
     utils.is_exists_dir(log_dir, True)
 
     prefix_out_file = "zudello_intrfc_"
-    extension_for_in_files = 'json'
+    #extension_for_in_files = 'json'
 
     log_filename = os.path.join(log_dir, 'error.log')
     logging.basicConfig(filename=log_filename,
@@ -84,12 +92,16 @@ def main():
                         datefmt='%m/%d/%Y %I:%M:%S %p')
     logger = logging.getLogger()
 
-    in_files = [file for file in os.listdir(in_dir)]
+    in_files = os.listdir(in_dir)
     for in_file in in_files:
         print("File: {}".format(in_file))
         json_content = utils.get_json_from_file(os.path.join(in_dir, in_file), logger)
         if json_content:
-            out_interface = convert_to_interface(json_content, logger)
+            out_interface = generate_out_interface(json_content, [])
+            # for k, v in generator_node(json_content):
+            #     print("{} - {}".format(k, v))
+            # out_interface = convert_to_interface(json_content)
+            in_file = add_extension(in_file, 'json')
             out_file_name = os.path.join(out_dir, prefix_out_file + in_file)
             utils.dump_json(out_file_name, out_interface)
 
